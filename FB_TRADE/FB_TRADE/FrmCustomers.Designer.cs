2769cf40256c0e5965e696b7df785514e35d9c47
logan.xu <logan.xu@anker.com>
2019-03-09 13:51:50 +0800

    aa

--- FB_TRADE/FB_TRADE/FrmCustomers.Designer.cs
+++ FB_TRADE/FB_TRADE/FrmCustomers.Designer.cs
@@ -28,19 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCustomers));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnCustomerAdd = new System.Windows.Forms.ToolStripButton();
+            this.panel = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnCustomerAdd});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1236, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnCustomerAdd
+            // 
+            this.btnCustomerAdd.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.btnCustomerAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnCustomerAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnCustomerAdd.Image")));
+            this.btnCustomerAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCustomerAdd.Margin = new System.Windows.Forms.Padding(0, 1, 5, 2);
+            this.btnCustomerAdd.Name = "btnCustomerAdd";
+            this.btnCustomerAdd.Size = new System.Drawing.Size(73, 24);
+            this.btnCustomerAdd.Text = "新增客户";
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(323, 44);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(200, 100);
+            this.panel.TabIndex = 1;
             // 
             // FrmCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1236, 527);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "FrmCustomers";
             this.Text = "客户管理";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnCustomerAdd;
+        private System.Windows.Forms.Panel panel;
     }
 }
